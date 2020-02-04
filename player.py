447bf77e975ac77620e7bf8139a07e01cea40d21
Fabian Jarrett <fabianjarrett@gmail.com>
2020-02-04 18:55:23 +0000

    Add platformer physics to player

--- player.py
+++ player.py
@@ -6,22 +6,27 @@ class Player(GameObject):
     def __init__(self, game, image, x=0, y=0):
         super().__init__(game, image, x, y)
         self.vel = 200
+        self.gravity = 1500
+        self.jump_force = 350
+        self.on_ground = False
         self.keyboard = None
         self.level_colliders = None
 
     def update(self, delta_time):
-        input_hor, input_ver = 0, 0
+        input_hor = 0
+        is_jumping = False
         if self.keyboard.is_key_down(Keyboard.UP):
-            input_ver -= 1
-        if self.keyboard.is_key_down(Keyboard.DOWN):
-            input_ver += 1
+            is_jumping = True
         if self.keyboard.is_key_down(Keyboard.LEFT):
             input_hor -= 1
         if self.keyboard.is_key_down(Keyboard.RIGHT):
             input_hor += 1
 
         self.vel_x = input_hor * self.vel
-        self.vel_y = input_ver * self.vel
+        self.vel_y += self.gravity * delta_time
+
+        if is_jumping and self.on_ground:
+            self.vel_y = -self.jump_force
 
         move_x = self.vel_x * delta_time
         move_y = self.vel_y * delta_time
@@ -32,10 +37,14 @@ class Player(GameObject):
                 move_x = hit["gap_x"]
         self.x += move_x
 
+        self.on_ground = False
         for collider in self.level_colliders:
             hit = self.collider.get_overlapping(collider, 0, move_y)
             if hit:
+                if move_y > 0:
+                    self.on_ground = True
                 move_y = hit["gap_y"]
+                self.vel_y = 0
         self.y += move_y
 
         self.collider.x, self.collider.y = self.x, self.y
